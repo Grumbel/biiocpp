fd931bd1b79c36ba301c3aba4e07cf6aec136d7e
Ingo Ruhnke <grumbel@gmail.com>
2022-08-21 18:36:25 +0200

    Add cross-compile support to Nix flake

--- flake.nix
+++ flake.nix
@@ -3,33 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
-    tinycmmc.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, tinycmmc }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages = flake-utils.lib.flattenTree {
+  outputs = { self, nixpkgs, tinycmmc }:
+    tinycmmc.lib.eachSystemWithPkgs (pkgs:
+      {
+        packages = rec {
+          default = biiocpp;
+
           biiocpp = pkgs.stdenv.mkDerivation {
             pname = "biiocpp";
             version = "0.0.0";
+
             src = nixpkgs.lib.cleanSource ./.;
+
             nativeBuildInputs = [
-              pkgs.cmake
+              pkgs.buildPackages.cmake
             ];
+
             buildInputs = [
-              tinycmmc.defaultPackage.${system}
+              tinycmmc.packages.${pkgs.system}.default
 
               pkgs.gtest
             ];
            };
         };
-        defaultPackage = packages.biiocpp;
-      });
+      }
+    );
 }
