66ccfd9c37981a2e857fe50a4f5fcaa182feebfa
Ingo Ruhnke <grumbel@gmail.com>
2023-05-24 22:09:09 +0200

    Update flake.nix to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Binary file I/O";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
